a1aa881e7639bb34fd261349daed3babfb26a142
YevgeniyShevchenko <yevgeniy.shevchenko@bureau.kyiv.ua>
2022-05-17 22:58:29 +0300

    microservices example

--- src/scripts/components/copy-text.js
+++ src/scripts/components/copy-text.js
@@ -1,19 +1,11 @@
 (() => {
-	const container = document.getElementsByClassName('copy-container')
+	const container = document.getElementsByClassName('copy')
 
 	Array.from(container).forEach(item => {
-		const btnContainer = item.getElementsByClassName('copy-btn-container')[0]
-		const btn = btnContainer.getElementsByClassName('copy-btn')[0]
-		const successText = btnContainer.getElementsByClassName('copy-notification-text')[0]
-		const text = item.getElementsByClassName('copy-text')[0]
+		const btn = item.getElementsByClassName('copy__button')[0]
+		const text = item.getElementsByClassName('copy__text')[0]
 		btn.addEventListener('click', () => {
-			window.navigator.clipboard.writeText(text.textContent).then(
-				() => {
-					console.log('Copied')
-				}, () => {
-					console.log('Nea')
-				}
-			)
+			window.navigator.clipboard.writeText(text.textContent)
 		})
 	})
 })()
